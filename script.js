61f4cdbafee1d9398c508cedd18b108f7c322c21
Shuriky <hdoo0027@student.monash.edu>
2025-05-07 21:20:07 +1000

    finally something that work

--- script.js
+++ script.js
@@ -1,5 +1,2 @@
-import { evaluate } from 'https://cdn.jsdelivr.net/npm/mathjs/lib/esm/index.js';
-
-const expression = "2 + 3 * (4 - 1)";
-const result = evaluate(expression);
-console.log(result);  // Output: 11
+const result = math.evaluate("2 + 3 * (4 - 1)");
+alert(result); // Output: 11
