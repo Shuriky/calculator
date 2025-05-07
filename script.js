60b32d739ad8163c5331027f877d41fc97ee368d
Shuriky <hdoo0027@student.monash.edu>
2025-05-07 21:12:10 +1000

    test again

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { evaluate } from 'mathjs';
+import { evaluate } from 'https://cdn.jsdelivr.net/npm/mathjs/lib/esm/index.js';
 
 const expression = "2 + 3 * (4 - 1)";
 const result = evaluate(expression);
