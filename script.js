1d1e65aa5493665e5b98f61ec67ac42bc8c78230
Shuriky <hdoo0027@student.monash.edu>
2025-05-10 03:31:05 +1000

    code cleanups also fix a few minor bugs

--- script.js
+++ script.js
@@ -1,169 +1,169 @@
-
-
 let display = document.querySelector(".output");
 let outputContent = "";
 let calculateContent = "";
 let result = 0; // Latest answer
-let scope = {ans: result}; // Support ANS
+let scope = { ans: result }; // Support ANS
 
 function updateDisplay() {
-  display.textContent = outputContent;
-  scope = {ans: result};
+    display.textContent = outputContent;
+    scope = { ans: result };
 }
 
 document.querySelector("#seven").addEventListener("click", function () {
-  outputContent += "7";
-  calculateContent += "7";
-  updateDisplay();
+    outputContent += "7";
+    calculateContent += "7";
+    updateDisplay();
 });
 
 document.querySelector("#eight").addEventListener("click", function () {
-  outputContent += "8";
-  calculateContent += "8";
-  updateDisplay();
+    outputContent += "8";
+    calculateContent += "8";
+    updateDisplay();
 });
 
 document.querySelector("#nine").addEventListener("click", function () {
-  outputContent += "9";
-  calculateContent += "9";
-  updateDisplay();
+    outputContent += "9";
+    calculateContent += "9";
+    updateDisplay();
 });
 
 document.querySelector("#left-bracket").addEventListener("click", function () {
-  outputContent += "(";
-  calculateContent += "(";
-  updateDisplay();
+    outputContent += "(";
+    calculateContent += "(";
+    updateDisplay();
 });
 
 document.querySelector("#right-bracket").addEventListener("click", function () {
-  outputContent += ")";
-  calculateContent += ")";
-  updateDisplay();
+    outputContent += ")";
+    calculateContent += ")";
+    updateDisplay();
 });
 
 document.querySelector("#ac").addEventListener("click", function () {
-  outputContent = "0";
-  calculateContent = "";
-  updateDisplay();
-  outputContent = "";
+    outputContent = "0";
+    calculateContent = "";
+    updateDisplay();
+    outputContent = "";
 });
 
 document.querySelector("#four").addEventListener("click", function () {
-  outputContent += "4";
-  calculateContent += "4";
-  updateDisplay();
+    outputContent += "4";
+    calculateContent += "4";
+    updateDisplay();
 });
 
 document.querySelector("#five").addEventListener("click", function () {
-  outputContent += "5";
-  calculateContent += "5";
-  updateDisplay();
+    outputContent += "5";
+    calculateContent += "5";
+    updateDisplay();
 });
 
 document.querySelector("#six").addEventListener("click", function () {
-  outputContent += "6";
-  calculateContent += "6";
-  updateDisplay();
+    outputContent += "6";
+    calculateContent += "6";
+    updateDisplay();
 });
 
 document.querySelector("#multiply").addEventListener("click", function () {
-  outputContent += "x";
-  calculateContent += "*";
-  updateDisplay();
+    outputContent += "x";
+    calculateContent += "*";
+    updateDisplay();
 });
 
 document.querySelector("#divide").addEventListener("click", function () {
-  outputContent += "÷";
-  calculateContent += "/";
-  updateDisplay();
+    outputContent += "÷";
+    calculateContent += "/";
+    updateDisplay();
 });
 
 document.querySelector("#power").addEventListener("click", function () {
-  outputContent += "^";
-  calculateContent += "^";
-  updateDisplay();
+    outputContent += "^";
+    calculateContent += "^";
+    updateDisplay();
 });
 
 document.querySelector("#one").addEventListener("click", function () {
-  outputContent += "1";
-  calculateContent += "1";
-  updateDisplay();
+    outputContent += "1";
+    calculateContent += "1";
+    updateDisplay();
 });
 
 document.querySelector("#two").addEventListener("click", function () {
-  outputContent += "2";
-  calculateContent += "2";
-  updateDisplay();
+    outputContent += "2";
+    calculateContent += "2";
+    updateDisplay();
 });
 
 document.querySelector("#three").addEventListener("click", function () {
-  outputContent += "3";
-  calculateContent += "3";
-  updateDisplay();
+    outputContent += "3";
+    calculateContent += "3";
+    updateDisplay();
 });
 
 document.querySelector("#add").addEventListener("click", function () {
-  outputContent += "+";
-  calculateContent += "+";
-  updateDisplay();
+    outputContent += "+";
+    calculateContent += "+";
+    updateDisplay();
 });
 
 document.querySelector("#subtract").addEventListener("click", function () {
-  outputContent += "-";
-  calculateContent += "-";
-  updateDisplay();
+    outputContent += "-";
+    calculateContent += "-";
+    updateDisplay();
 });
 
 document.querySelector("#sqrt").addEventListener("click", function () {
-  outputContent += "√(";
-  calculateContent += "sqrt(";
-  updateDisplay();
+    outputContent += "√(";
+    calculateContent += "sqrt(";
+    updateDisplay();
 });
 
 document.querySelector("#zero").addEventListener("click", function () {
-  outputContent += "0";
-  calculateContent += "0";
-  updateDisplay();
+    outputContent += "0";
+    calculateContent += "0";
+    updateDisplay();
 });
 
 document.querySelector("#dot").addEventListener("click", function () {
-  outputContent += ".";
-  calculateContent += ".";
-  updateDisplay();
+    outputContent += ".";
+    calculateContent += ".";
+    updateDisplay();
 });
 
 document.querySelector("#triplezero").addEventListener("click", function () {
-  outputContent += "000";
-  calculateContent += "000";
-  updateDisplay();
+    outputContent += "000";
+    calculateContent += "000";
+    updateDisplay();
 });
 
-// ts trippin
 document.querySelector("#ans").addEventListener("click", function () {
-  // You could store the previous result in a variable like `lastAnswer`
-  outputContent += "ANS";
-  calculateContent += "ans";
-  updateDisplay();
+    outputContent += "ANS";
+    calculateContent += "ans";
+    updateDisplay();
 });
 
 document.querySelector("#delete").addEventListener("click", function () {
-  outputContent = outputContent.slice(0, -1);
-  calculateContent = calculateContent.slice(0, -1);
-  console.log(calculateContent);
-  console.log(outputContent);
-  updateDisplay();
+    outputContent = outputContent.slice(0, -1);
+    calculateContent = calculateContent.slice(0, -1);
+    // 
+    console.log(calculateContent.toString().length);
+    if (calculateContent.toString().length === 0) {
+        calculateContent = "0";
+        outputContent = "0";
+    }
+    updateDisplay();
 });
 
 document.querySelector("#equal").addEventListener("click", function () {
     calculateContent = "(" + calculateContent + ")";
     console.log(calculateContent);
-    console.log(outputContent);
-  try {
-    outputContent = math.evaluate(calculateContent, scope).toString(); // simple power support
-    result = parseInt(outputContent); // Store result for ANS
-    calculateContent = result.toString();
-  } catch {
-    outputContent = "Syntax Error";
-  }
-  updateDisplay();
+    try {
+        outputContent = math.evaluate(calculateContent, scope).toString();
+        result = parseFloat(outputContent); // Store result for ANS
+        calculateContent = result.toString();
+        console.log("result: " + outputContent);
+    } catch {
+        outputContent = "Syntax Error";
+    }
+    updateDisplay();
 });
